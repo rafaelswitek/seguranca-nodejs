a45993afb87d57ca263a92d4a048573e24cfad73
Rafael Gonçalves <rafaelswitek@gmail.com>
2023-10-12 16:31:05 -0300

    02. Implementando a autenticação

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,10 +2,12 @@ const bodyParser = require('body-parser')
  
 const produto = require('./produtoRoute')
 const usuario = require('./usuarioRoute')
+const auth = require('./authRoute')
 
 module.exports = app => {
   app.use(
     bodyParser.json(),
+    auth,
     produto,
     usuario,
   )
