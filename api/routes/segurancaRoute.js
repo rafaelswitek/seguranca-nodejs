8b563132b072312339b1b6beb4792fbbb2eafa1a
Rafael Gonçalves <rafaelswitek@gmail.com>
2023-10-12 18:16:19 -0300

    04. Cadastro de perfis e permissões

---
+++ api/routes/segurancaRoute.js
@@ -0,0 +1,12 @@
+const { Router } = require('express')
+const SegurancaController = require('../controllers/segurancaController')
+
+const router = Router()
+
+
+router
+    .post('/seguranca/acl', SegurancaController.cadastrarAcl)
+    .post('/seguranca/permissoes-roles', SegurancaController.cadastrarPermissoesRoles)
+
+
+module.exports = router
